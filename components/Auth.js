1916b2c0ef69966b6bbe69f70b70499fdc883090
Syunsuke Shima <s.syun617@icloud.com>
2023-12-03 11:12:38 +0900

    🚑

--- components/Auth.js
+++ components/Auth.js
@@ -105,6 +105,7 @@ export const Auth = () => {
                   placeholder="Password"
                   autoComplete="current-password"
                   required
+                  value={password}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={(e) => {
                     setPassword(e.target.value);
